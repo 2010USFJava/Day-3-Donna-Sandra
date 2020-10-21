792c567015486eae63f0e225ffffaf58d1d1bf56
unknown <dbond.tech@gmail.com>
2020-10-21 16:13:18 -0500

    individual branch

--- PartnerFunWithSandraAndDonna/src/AllTheFun/Driver.java
+++ PartnerFunWithSandraAndDonna/src/AllTheFun/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		Student aStudent= new Student("Bob", 95);
 		Class myClass = new Class(aStudent, "Fun With Java", 254);
-		System.out.println("woo on main branch");
+		System.out.println("woo on Donna branch");
 		System.out.println(myClass.toString());
 	}
 
